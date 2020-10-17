3cf6d0173197a1b21d6e88e8b45554f7dd517bf7
Emmanuel Najfar <if20b200@technikum-wien.at>
2020-10-17 11:30:44 +0200

    files

---
+++ src/User.java
@@ -0,0 +1,30 @@
+public class User {
+    private String username;
+    private String password;
+    private int coins;
+    private Card[] stack;
+    private Card[] deck;
+
+    public void acquirePackages() {
+        if (coins >= 4) {
+        }
+    }
+
+    public void register() {
+
+    }
+
+    public void login() {
+
+    }
+
+    public void defineDeck() {
+
+    }
+
+    public void battle() {
+
+    }
+
+
+}
