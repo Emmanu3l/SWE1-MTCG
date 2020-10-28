b65dc643ecd13fbc7b350bcc857eb176acc7f0c8
Emmanuel Najfar <if20b200@technikum-wien.at>
2020-10-28 10:01:58 +0100

    implementing client and server

--- src/ClientServer/Server.java
+++ src/ClientServer/Server.java
@@ -1,13 +1,15 @@
 package ClientServer;
-import java.net.*;
 import java.io.*;
-
+import java.net.*;
 
 public class Server {
     //register and Login
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(8000);
+            Socket socket = serverSocket.accept();
+
+            System.out.println("connected");
         } catch (IOException e) {
             e.printStackTrace();
         }
