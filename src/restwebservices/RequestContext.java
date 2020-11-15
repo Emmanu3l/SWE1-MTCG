093bf2e3a33fe246454c7d197bc1f7f28975e35d
Emmanuel Najfar <if20b200@technikum-wien.at>
2020-11-15 13:34:44 +0100

    add parsing

--- src/restwebservices/RequestContext.java
+++ src/restwebservices/RequestContext.java
@@ -1,6 +1,20 @@
 package restwebservices;
 
 
-public enum RequestContext {
+import java.util.LinkedList;
+
+public class RequestContext {
     //GET, POST, PUT, DELETE
+    private String verb;
+    private String URI;
+    private String version;
+    private String headers;
+
+    public RequestContext(String verb, String URI, String version, String headers) {
+        this.verb = verb;
+        this.URI = URI;
+        this.version = version;
+        this.headers = headers;
+    }
+
 }
--- src/restwebservices/WebserviceHandler.java
+++ src/restwebservices/WebserviceHandler.java
@@ -1,12 +1,39 @@
 package restwebservices;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class WebserviceHandler {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String request = scanner.nextLine();
+        System.out.println(request);
+        RequestContext response = parseRequest(request);
+        System.out.println(response);
     }
 
-    public void parseHTTP() {
+    //parse the http request's header containing the following information
+    public static RequestContext parseRequest(String request) {
+        String[] parsedRequest = request.trim().split(" ");
+        //parse header
+        //request line: verb, URI, Version
+        //optional request headers (name:value, ...)
+        //check for blank line, then call parseBody
+        String verb = parsedRequest[0];
+        System.out.println(verb);
+        String URI = parsedRequest[1];
+        System.out.println(URI);
+        String version = parsedRequest[2];
+        System.out.println(version);
+        //new line
+        String headers = "";
 
+        //blank line
+
+        //parse body
+        //optional body with additional information for the server
+
+        return new RequestContext(verb, URI, version, headers);
     }
 
 }
